f2dafa2885508253881242c931e4ff9115c0cece
07-donny <49312038+07-donny@users.noreply.github.com>
2020-05-19 10:42:26 +0200

    Add project files.

---
+++ MC1000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MC1000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
