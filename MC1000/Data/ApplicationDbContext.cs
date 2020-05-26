24a84b70c3a8ac23772fa91ea4ae58f815a3e56a
07-donny <49312038+07-donny@users.noreply.github.com>
2020-05-26 11:18:28 +0200

    appdb

--- MC1000/Data/ApplicationDbContext.cs
+++ MC1000/Data/ApplicationDbContext.cs
@@ -14,15 +14,28 @@ namespace MC1000.Data
         {
         }
 
-            //public DbSet<Category> Category { get; set; }
-            public DbSet<DeliverySlot> DeliverySlot { get; set; }
-            public DbSet<Discount> Discount { get; set; }
-            public DbSet<News> News { get; set; }
-            public DbSet<Order> Order { get; set; }
-            public DbSet<OrderLine> OrderLine { get; set; }
-            public DbSet<Product> Product { get; set; }
-            public DbSet<Promotion> Promotion { get; set; }
-            public DbSet<ShoppingCart> ShoppingCart { get; set; }
-            public DbSet<TimeSlot> TimeSlot { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>()
+                .HasMany(p => p.SubCategories)
+                .WithOne(c => c.Category)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<SubCategory>()
+            .HasMany(p => p.SubSubCategories)
+            .WithOne(c => c.SubCategory)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
+        public DbSet<DeliverySlot> DeliverySlot { get; set; }
+        public DbSet<Discount> Discount { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderLine> OrderLine { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Promotion> Promotion { get; set; }
+        public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public DbSet<TimeSlot> TimeSlot { get; set; }
+        public DbSet<Category> Category { get; set; }
     }
 }
