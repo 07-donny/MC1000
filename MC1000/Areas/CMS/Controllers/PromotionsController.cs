e279beab19c41b52be360856dd80c8bd4505d6fd
07-donny <49312038+07-donny@users.noreply.github.com>
2020-06-03 13:13:31 +0200

    discount overzicht per promotie

--- MC1000/Areas/CMS/Controllers/PromotionsController.cs
+++ MC1000/Areas/CMS/Controllers/PromotionsController.cs
@@ -20,7 +20,7 @@ namespace MC1000.Areas.CMS.Controllers
             _context = context;
         }
 
-        // GET: CMS/Promotions
+        // Promotie beheer
         public async Task<IActionResult> Index()
         {
             return View(await _context.Promotion.ToListAsync());
@@ -145,6 +145,126 @@ namespace MC1000.Areas.CMS.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        //Discount beheer
+        public async Task<IActionResult> Discounts(int? id) //index
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var promotion = await _context.Promotion
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            List<Discount> discountList = new List<Discount>();
+            foreach (var d in _context.Discount)
+            {
+                if (d.PromotionId == id)
+                {
+                    discountList.Add(d);
+                }
+            }
+
+            return View("Discounts", discountList);
+        }
+
+        public IActionResult DiscountCreate()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DiscountCreate([Bind("Id,DiscountedPrice,ValidUntil,PromotionId,EAN")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var promotion = await _context.Promotion.FindAsync(id);
+        //    if (promotion == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(promotion);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Title")] Promotion promotion)
+        //{
+        //    if (id != promotion.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(promotion);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!PromotionExists(promotion.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(promotion);
+        //}
+
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var promotion = await _context.Promotion
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (promotion == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(promotion);
+        //}
+
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var promotion = await _context.Promotion.FindAsync(id);
+        //    _context.Promotion.Remove(promotion);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool PromotionExists(int id)
         {
